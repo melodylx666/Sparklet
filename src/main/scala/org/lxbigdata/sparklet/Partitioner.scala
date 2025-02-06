49238e6b6f24b45706c242f930085e499fc33a16
melodylx <2135929866@qq.com>
2025-02-06 20:38:56 +0800

    feat(core): 添加自定义分区器实现
    - 新增 Partitioner抽象类，定义分区器接口
    - 实现 HashPartitioner 类，提供哈希分区功能
    - 为后续的 RDD分区处理和优化奠定基础

---
+++ src/main/scala/org/lxbigdata/sparklet/Partitioner.scala
@@ -0,0 +1,35 @@
+package org.lxbigdata.sparklet
+
+/**
+ * ClassName: Partitioner
+ * Package: org.lxbigdata.sparklet
+ * Description: 分区器，以及哈希分区器实现
+ *
+ * @author lx
+ * @version 1.0   
+ */
+abstract class Partitioner {
+  def numPartitions: Int
+  def getPartition(key: Any): Int
+}
+
+class HashPartitioner(partitions: Int) extends Partitioner {
+  //获取分区总数
+  override def numPartitions: Int = partitions
+  //计算key的分区
+  override def getPartition(key: Any): Int = key match {
+    case x if x != null => {
+      val k = x.hashCode()
+      val rawMod = k % partitions
+      rawMod +(if (rawMod < 0) numPartitions  else 0)
+    }
+    case _ => 0
+  }
+  //判断分区器是否相等
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case hp: HashPartitioner => hp.numPartitions == numPartitions
+      case _ => false
+    }
+  }
+}
